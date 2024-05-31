ca32011905a11391cd31aa143890eb238675895e
Naeun Kim <102296721+Nico1eKim@users.noreply.github.com>
2024-05-31 14:25:45 +0900

    feat: 9주차 미션 완료

--- app/src/main/java/com/example/a9weeks/Home.kt
+++ app/src/main/java/com/example/a9weeks/Home.kt
@@ -25,6 +25,44 @@ class Home : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        val retrofit = RetrofitService.retrofit
+
+        val service = retrofit.create(RetrofitIF::class.java)
+
+        service.getPosts().enqueue(object : Callback<BaseData<PostResult>> {
+            override fun onResponse(call: Call<BaseData<PostResult>>, response: Response<BaseData<PostResult>>) {
+                if(response.isSuccessful){
+                    val response = response.body()
+
+                    if (response != null) {
+                        Glide.with(requireContext())
+                            .load(response.result.postImage)
+                            .into(binding.ivHomePostingImage)
+
+                        Glide.with(requireContext())
+                            .load(response.result.userProfileImage)
+                            .into(binding.sivHomePostingProfile)
+
+                        binding.tvHomePostingProfileName.text = response.result.nickName
+                        binding.tvHomePostingInfo.text = response.result.nickName
+                        binding.tvHomePostingContentName.text = response.result.nickName
+                        binding.tvHomePostingContent.text = response.result.nickName
+
+                    }
+
+                    if (response != null) {
+                        Log.d("성공", response.result.postImage)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<BaseData<PostResult>>, t: Throwable) {
+                Log.d("실패", t.message.toString())
+            }
+
+        })
+
         return binding.root
     }
 
